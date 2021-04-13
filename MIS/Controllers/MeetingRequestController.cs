4a1083b65296f703970adf80c039f7e9b62c550a
mihnea9923 <54399158+mihnea9923@users.noreply.github.com>
2021-04-13 13:33:34 +0300

    request controller

---
+++ MIS/Controllers/MeetingRequestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIS.Controllers
+{
+    public class MeetingRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(MeetingRequest meetingRequest , List<string> participants , string test)
+        {
+
+            return Ok();
+        }
+
+    }
+}
--- MIS/wwwroot/js/site.js
+++ MIS/wwwroot/js/site.js
@@ -1,9 +1,13 @@
-﻿function addParticipant(event, value) {
+﻿var participantIndex = 0
+function addParticipant(event, value) {
     if (event.keyCode == 13) {
         event.preventDefault()
         var listItem = `<li class="participant" onclick="this.remove()">
-        ${value} <i class="fas fa-times"></i></li > `
-        $('#participantsList').append(listItem)
+        ${value} <i class="fas fa-times"></i></li >`;
+        $('#participantsList').append(listItem);
+        $('#participantsList').append(`<input type="hidden" name="participants[${participantIndex}]" />`);
+        participantIndex++;
+        $('#participantEmail').val('');
 
     }
 }
